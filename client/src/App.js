2b1dddbf053677dc07256f79451c65d3825e0680
MuhammadHuzaif607 <128931955+MuhammadHuzaif607@users.noreply.github.com>
2023-12-09 04:40:01 -0800

    uploading server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './styles/App.css';
+import SignIn from './components/Signinform/signin';
 import Admin from './panels/admin/admin';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Admin />
+      <Routes>
+        <Route path="/" element={<SignIn />}></Route>
+        <Route path="/student" element={<Admin />}></Route>
+      </Routes>
     </div>
   );
 }
