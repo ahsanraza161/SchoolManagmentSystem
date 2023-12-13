dd760bbb64b14278ef207196403864abd8d5287e
ahsanraza161 <ahsanrazainfo.com>
2023-12-13 18:51:47 +0500

    updating Admin panel

--- client/src/panels/admin/admin.jsx
+++ client/src/panels/admin/admin.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import Dashboard from './components/dashboard'
+import React, { useState} from 'react'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Home from './components/Home'
 import '../../styles/admin.css'
 
 function Admin() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
   return (
-    <div>
-      <Dashboard/>
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
     </div>
   )
 }
