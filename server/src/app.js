31539d528e44b7ce5028fafa52075b30cbb24fda
ahsanraza161 <ahsanrazainfo.com>
2023-12-17 15:59:03 +0500

    checking backend is connected or not

--- server/src/app.js
+++ server/src/app.js
@@ -4,35 +4,21 @@ import { createServer } from 'http';
 import bodyParser from 'body-parser';
 import { connectDB } from './DB/index.js';
 import { AuthRouters } from './Router/AuthRouters.js';
+let app = express();
 
-const app = express();
-const PORT = process.env.PORT || 3050; // Use process.env.PORT if available
+var corsOptions = {
+  origin: '*',
+};
+app.use(cors(corsOptions));
+app.use(cors({ origin: '*' }));
+app.use(bodyParser.json());
+app.use(AuthRouters);
+const httpServer = createServer(app);
+const PORT = 3050;
 
-// Connect to the database
+//connecting to database
 connectDB();
 
-// Use bodyParser for parsing JSON
-app.use(bodyParser.json());
-
-// Set up CORS
-const corsOptions = {
-  origin: '*',
-// some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-app.use(cors(corsOptions));
-
-// Define routes
-app.get('/', (req, res) => {
-  res.json('Hello');
-});
-
-// Use the AuthRouters
-app.use(AuthRouters);
-
-// Create an HTTP server and listen to the specified port
-const httpServer = createServer(app);
-
 httpServer.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`server is running on port ${PORT}`);
 });
