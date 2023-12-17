91c049503909ecbfc67f0c805d41688778986680
ahsanraza161 <ahsanrazainfo.com>
2023-12-17 14:19:23 +0500

    setting app for delpoymint

--- server/src/app.js
+++ server/src/app.js
@@ -4,31 +4,37 @@ import { createServer } from 'http';
 import bodyParser from 'body-parser';
 import { connectDB } from './DB/index.js';
 import { AuthRouters } from './Router/AuthRouters.js';
-let app = express();
-app.use(
-  cors({
-    origin: ['https://smsfrontend-nine.vercel.app'],
-    methods: ['POST', 'GET'],
-    credentials: true,
-  })
-);
 
+const app = express();
+const PORT = process.env.PORT || 3050; // Use process.env.PORT if available
+
+// Connect to the database
+connectDB();
+
+// Use bodyParser for parsing JSON
+app.use(bodyParser.json());
+
+// Set up CORS
+const corsOptions = {
+  origin: 'https://smsfrontend-nine.vercel.app',
+  methods: ['POST', 'GET'],
+  credentials: true,
+  optionsSuccessStatus: 204, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+// Define routes
 app.get('/', (req, res) => {
   res.json('Hello');
 });
-var corsOptions = {
-  origin: '*',
-};
-app.use(cors(corsOptions));
-app.use(cors({ origin: '*' }));
-app.use(bodyParser.json());
-app.use(AuthRouters);
-const httpServer = createServer(app);
-const PORT = 3050;
 
-//connecting to database
-connectDB();
+// Use the AuthRouters
+app.use(AuthRouters);
+
+// Create an HTTP server and listen to the specified port
+const httpServer = createServer(app);
 
 httpServer.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
